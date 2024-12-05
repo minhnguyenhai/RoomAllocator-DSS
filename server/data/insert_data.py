5cc7947061046a70b73b70c449e07764dcd003ac
haiminhnguyenn <haiminhc2dh@gmail.com>
2024-12-05 22:14:03 +0700

    Refactor: Update data for new schema

--- server/data/insert_data.py
+++ server/data/insert_data.py
@@ -2,17 +2,7 @@ import json
 import random
 from faker import Faker
 from app import create_app, db
-from app.models.dormitory import Dormitory
 from app.models.student_request import StudentRequest
-
-
-fake = Faker()
-
-
-def load_dormitories_from_json():
-    with open("./data/dormitory_data.json", "r", encoding="utf-8") as file:
-        data = json.load(file)
-    return data
 
 
 def load_names_from_json():
@@ -21,67 +11,73 @@ def load_names_from_json():
     return data["first_names"], data["male_last_names"], data["female_last_names"]
 
 
-dormitories_data = load_dormitories_from_json()
+faker = Faker()
 first_names, male_last_names, female_last_names = load_names_from_json()
 
-
-def generate_dormitory(dormitory_data):
-    return Dormitory(
-        building_name=dormitory_data["building_name"],
-        total_floors=dormitory_data["total_floors"],
-        rooms_per_floor=dormitory_data["rooms_per_floor"],
-        rent_per_month=dormitory_data["rent_per_month"],
-        people_per_room=dormitory_data["people_per_room"],
-        private_toilet=dormitory_data["private_toilet"],
-        water_heater=dormitory_data["water_heater"],
-        air_conditioner=dormitory_data["air_conditioner"]
-    )
+genders = ["Nam", "Nữ"]
+bed_time_habits = ["Sớm", "Bình thường", "Muộn"]
+social_stypes = ["Hướng nội", "Hướng ngoại", "Bình thường"]
+religions = ["Có", "Không"]
+academic_years = [66, 67, 68, 69]
+majors = ["CNTT&TT", "Cơ khí", "Điện - Điện tử", "Kinh tế", "Ngoại ngữ"]
+sports_passions = ["Có", "Không", "Bình thường"]
+music_passions = ["Có", "Không", "Bình thường"]
+gaming_passions = ["Có", "Không", "Bình thường"]
 
 
-def generate_student_request():
-    year = random.choice([2021, 2022, 2023])
-    random_number = fake.unique.random_number(digits=4)
-    student_id = f"{year}{random_number:04d}"
-    gender = random.choice(["male", "female"])
-    first_name = random.choice(first_names)
-    if gender == "male":
-        last_name = random.choice(male_last_names)
-    else:
-        last_name = random.choice(female_last_names)
+def generate_student_requests_data(num_of_records, seed=42):
+    data = []
+    faker.seed_instance(42)
+    random.seed(seed)
+    
+    for _ in range(num_of_records):
+        academic_year = random.choices(academic_years, [0.1, 0.2, 0.3, 0.4])[0]
+        if academic_year == 66:
+            entry_year = 2021
+        elif academic_year == 67:
+            entry_year = 2022
+        elif academic_year == 68:
+            entry_year = 2023
+        else:
+            entry_year = 2024
+            
+        random_number = faker.unique.random_number(digits=4)
+        student_id = f"{entry_year}{random_number:04d}"
+
+        gender = random.choices(genders, [0.6, 0.4])[0]
+        first_name = random.choice(first_names)
+        if gender == "Nam":
+            last_name = random.choice(male_last_names)
+        else:
+            last_name = random.choice(female_last_names)
+            
+        full_name = f"{first_name} {last_name}"
         
-    full_name = f"{first_name} {last_name}"
+        student_request = StudentRequest(
+            student_id=student_id,  
+            name=full_name,
+            gender=gender,
+            bed_time_habit=random.choices(bed_time_habits, [0.15, 0.5, 0.35])[0],
+            social_style=random.choices(social_stypes, [0.25, 0.5, 0.25])[0],
+            religion=random.choices(religions, [0.2, 0.8])[0],
+            academic_year=academic_year,
+            major=random.choices(majors, [0.25, 0.2, 0.2, 0.2, 0.15])[0],
+            sports_passion=random.choices(sports_passions, [0.5, 0.2, 0.3])[0],
+            music_passion=random.choices(music_passions, [0.4, 0.4, 0.2])[0],
+            gaming_passion=random.choices(gaming_passions, [0.6, 0.1, 0.3])[0],
+            average_monthly_spending=random.randint(1500, 5000)*1000
+        )
+        data.append(student_request)
+        
+    return data
 
-    return StudentRequest(
-        student_id=student_id,  
-        name=full_name,
-        gender=gender,
-        rent_per_month=(fake.random_int(min=300, max=600))*1000,
-        people_per_room=fake.random_int(min=6, max=12),
-        private_toilet=fake.random_int(min=1, max=2),
-        water_heater=fake.random_int(min=0, max=2),
-        air_conditioner=fake.random_int(min=0, max=2),
-        primary_study_time=fake.random_element(elements=("morning", "afternoon", "evening")),
-        social_style=fake.random_element(elements=("introvert", "extrovert", "balanced")),
-        silent_space_required=fake.boolean(),
-        bed_time_habit=fake.random_element(elements=("early", "late")),
-        is_smoker=fake.boolean()
-    )
 
-
-def insert_data_into_database(num_of_students=2500):
+def insert_data_into_database(num_of_students=2000):
     app = create_app()
     with app.app_context():
-        if Dormitory.query.first() is None:
-            dormitories = [generate_dormitory(data) for data in dormitories_data]
-            db.session.bulk_save_objects(dormitories)
-            db.session.commit()
-            print(f"{len(dormitories)} records of dormitories data inserted into database.")
-        else:
-            print("Data for dormitories already exist in the database.")
-            
         if StudentRequest.query.first() is None:
-            students = [generate_student_request() for _ in range(num_of_students)]
-            db.session.bulk_save_objects(students)
+            student_requests_data = generate_student_requests_data(num_of_students)
+            db.session.bulk_save_objects(student_requests_data)
             db.session.commit()
             print(f"{num_of_students} records of student requests data inserted into database.")
         else:
